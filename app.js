1de39bd944bce6f0f0778b443326f3b89ed0b317
techhubhosting <82578836+techhubhosting@users.noreply.github.com>
2023-05-16 21:05:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+const observers = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('leftslideshow');
+        }
+        else{
+            entry.target.classList.remove('leftslideshow');
+        }
+    })
+})
+
+const hiddenElement = document.querySelectorAll('.leftslide');
+hiddenElement.forEach((el) => observers.observe(el));
+
+var cursor = document.querySelector(".cursor");
+    var cursor2 = document.querySelector(".cursor2");
+    document.addEventListener("mousemove",function(e){
+      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    });
+
+
+
